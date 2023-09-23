06ba81487a763ccb0d60caec8e4a63a15466a754
agnibhaxray <141936229+agnibhaxray@users.noreply.github.com>
2023-09-23 12:22:24 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request, redirect
+from flask.wrappers import Request
+from flask_sqlalchemy import SQLAlchemy, sqlalchemy
+from datetime import datetime
+
+from werkzeug.utils import redirect
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(200), nullable=False)
+    desript=db.Column(db.String(1000), nullable=False)
+    date_added=db.Column(db.DateTime, default=datetime.utcnow)
+
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+
+@app.route('/', methods=['GET','POST'])
+def hello_world():
+    if request.method=='POST':
+     title=request.form['title']
+     desript=request.form['desript']
+     todo= Todo(title=title, desript=desript)
+     db.session.add(todo)
+     db.session.commit()
+    allTodo=Todo.query.all()
+    return render_template('index.html', allTodo=allTodo)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo=Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
+@app.route('/update/<int:sno>', methods=['GET','POST'])
+def update(sno):
+    if request.method=='POST':
+         title=request.form['title']
+         desript=request.form['desript']
+         todo=Todo.query.filter_by(sno=sno).first()
+         todo.title=title
+         todo.desript=desript         
+         db.session.add(todo)
+         db.session.commit()
+         return redirect("/")
+    todo=Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo=todo)
+
+
+@app.route('/about')
+def about():
+     return render_template('about.html')
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True, port=5000)
+   
